e3a085cd2016af78a1c41d67ed40cb3472af3b50
VladimirPopkov <vovanpopkov@mail.ru>
2021-01-18 23:19:33 +0300

    Добавлены классы

---
+++ src/com/company/Figure.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public abstract class Figure {
+
+    private float x, y;
+
+    abstract void square();
+
+    Quadrant getQuadrant(){
+        return null;
+    }
+}
---
+++ src/com/company/Quadrant.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public enum Quadrant {
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH;
+}
